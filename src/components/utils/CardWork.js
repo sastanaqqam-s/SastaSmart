6d47bd531058f3b4e5c76a76149d65d040e3fb29
cdsali <cdsali0202@gmail.com>
2024-05-15 03:17:42 +0100

    upfate works

--- src/components/utils/CardWork.js
+++ src/components/utils/CardWork.js
@@ -6,14 +6,14 @@ const CardWork = ({data}) => {
   return (
    
     <Row className="card-work" id="crd">
-    <Col sm={7} md={8} className={data.order === 0 ? "order-md-1" : "order-md-2"} style={{paddingLeft:"50px"}}> 
+    <Col sm={7} md={8} className={data.order === 0 ? "order-md-1 cont" : "order-md-2 cont"} > 
       <h1 className='titles' style={{ fontSize: "1.8em",position:"relative" }}>
         {data.title}
 
    
       </h1>
     
-      <p className="home-about-body" style={{textAlign:"start"}}>
+      <p  style={{textAlign:"start"}}>
        {data.description}
       </p>
     </Col>
@@ -24,7 +24,7 @@ const CardWork = ({data}) => {
           src={require(`../../Assets/${data.image}`)}
           className="img-fluid"
           alt="avatar"
-          style={{maxHeight:"380px"}}
+         
           
         />
       
